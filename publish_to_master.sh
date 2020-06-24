fb3f168cd8b42c52050861c1cb294fd16e9f5f49
Ricardo G. Sousa <rsousa@rsousa.org>
2020-06-24 16:48:07 +0100

    chore: fix name to adapt to user pages rules

--- publish_to_master.sh
+++ publish_to_master.sh
@@ -12,8 +12,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,5 +22,5 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "feat: Publishing to gh-pages."
+cd public && git add --all && git commit -m "feat: Publishing to master."
 
